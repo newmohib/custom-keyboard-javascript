82df68f2b0c2788564ff9e292cd31b4dbf8bc2cc
mohibbs <newmohib@gmail.com>
2020-09-04 03:33:52 +0600

    add: cursor move add and css update

--- index.js
+++ index.js
@@ -10,33 +10,70 @@ let arr = [
     { type: "num", value: 7 ,backColor:"#FF6347"},
     { type: "num", value: 8 ,backColor:"#FF6347"},
     { type: "num", value: 9 ,backColor:"#FF6347"},
-    { type: "num", value: '*' ,backColor:"#FF6347"},
-    { type: "num", value: '#' ,backColor:"#FF6347"},
-    { type: "str", value: "mohib1", backColor:"#7FFFD4" },
-    { type: "str", value: "rony",   backColor:"#DEB887"  },
-    { type: "str", value: "mohib2" ,backColor:"#F5F5DC" },
-    { type: "str", value: "mohib3" ,backColor:"#FFE4C4" },
-    { type: "str", value: "mohib4" ,backColor:"#ADD8E6" },
-    { type: "str", value: "mohib5" ,backColor:"#8A2BE2" },
-    { type: "str", value: "mohib6" ,backColor:"#A52A2A" },
-    { type: "str", value: "mohib7" ,backColor:"#5F9EA0" },
-    { type: "str", value: "mohib8" ,backColor:"#DEB887" },
-    { type: "str", value: "mohib9" ,backColor:"#D2691E" },
-    { type: "str", value: "mohib10",backColor:"#FF7F50"  },
-    { type: "str", value: "mohib11",backColor:"#7FFFD4"  },
-    { type: "str", value: "mohib12",backColor:"#6495ED"  },
-    { type: "str", value: "mohib13",backColor:"#FFF8DC"  },
-    { type: "str", value: "mohib14",backColor:"#DC143C"  },
-    { type: "str", value: "mohib15",backColor:"#808000"  },
-    { type: "str", value: "mohib16",backColor:"#7FFFD4"  },
-    { type: "str", value: "mohib17",backColor:"#008B8B"  },
-    { type: "str", value: "mohib18",backColor:"#B8860B"  }
+   // { type: "num", value: '*' ,backColor:"#FF6347"},
+   // { type: "num", value: '#' ,backColor:"#FF6347"},
+    { type: "num", value: 'P' ,backColor:"#FF6347"},
+    { type: "num", value: 'M' ,backColor:"#FF6347"},
+    { type: "num", value: 'R' ,backColor:"#FF6347"},
+    { type: "str", value: "King", backColor:"#7FFFD4" },
+    { type: "str", value: "Queen",   backColor:"#DEB887"  },           
+    { type: "str", value: "Full" ,backColor:"#F5F5DC" },
+    { type: "str", value: "Twin" ,backColor:"#FFE4C4" },
+    { type: "str", value: "Bunk" ,backColor:"#ADD8E6" },
+    { type: "str", value: "Night" ,backColor:"#8A2BE2" },
+    { type: "str", value: "Dress" ,backColor:"#A52A2A" },
+    { type: "str", value: "Chest" ,backColor:"#5F9EA0" },
+    { type: "str", value: "Sec" ,backColor:"#DEB887" },
+    { type: "str", value: "Couch" ,backColor:"#D2691E" },
+    { type: "str", value: "Reclin",backColor:"#FF7F50"  },
+    { type: "str", value: "Hide",backColor:"#7FFFD4"  },
+    { type: "str", value: "End",backColor:"#6495ED"  },
+    { type: "str", value: "Coffee",backColor:"#FFF8DC"  },
+    { type: "str", value: "TV",backColor:"#DC143C"  },
+    { type: "str", value: "Stand",backColor:"#808000"  },
+    { type: "str", value: "Chair",backColor:"#7FFFD4"  },
+    { type: "str", value: "Lazy",backColor:"#008B8B"  },
+    { type: "str", value: "Book",backColor:"#B8860B"  },
+    { type: "str", value: "Shelf",backColor:"#B8860B"  },
+    { type: "str", value: "Book",backColor:"#B8860B"  },
+    { type: "str", value: "Dine",backColor:"#B8860B"  },
+    { type: "str", value: "China",backColor:"#B8860B"  },
+    { type: "str", value: "Cubby",backColor:"#B8860B"  },
+    { type: "str", value: "Shoe",backColor:"#B8860B"  },
+    { type: "str", value: "Pack",backColor:"#B8860B"  },
+    { type: "str", value: "Dump",backColor:"#B8860B"  },
+    { type: "str", value: "Bring",backColor:"#B8860B"  },
+    { type: "str", value: "Wrap",backColor:"#B8860B"  },
+    { type: "str", value: "File",backColor:"#B8860B"  },
+    { type: "str", value: "Stool",backColor:"#B8860B"  },
+    { type: "str", value: "Desk",backColor:"#B8860B"  },
+    { type: "str", value: "Hutch",backColor:"#B8860B"  },
+    { type: "str", value: "Platform-",backColor:"#B8860B"  },
+    { type: "str", value: "Mech-",backColor:"#B8860B"  },
+    { type: "str", value: "Boxes",backColor:"#B8860B"  },
+    { type: "str", value: "Bins",backColor:"#B8860B"  },
+    { type: "str", value: "Patio",backColor:"#B8860B"  },
+    { type: "str", value: "BBQ",backColor:"#B8860B"  },
+    { type: "str", value: "Plants",backColor:"#B8860B"  },
+    { type: "str", value: "Tool",backColor:"#B8860B"  },
+    { type: "str", value: "Fit",backColor:"#B8860B"  },
+    { type: "str", value: "Wood-",backColor:"#B8860B"  },
+    { type: "str", value: "Metal-",backColor:"#B8860B"  },
+    { type: "str", value: "Glass-",backColor:"#B8860B"  },
+    { type: "str", value: "Plastic-",backColor:"#B8860B"  },
+    { type: "str", value: "Heavy-",backColor:"#B8860B"  },
+    { type: "str", value: "Big-",backColor:"#B8860B"  },
+    { type: "str", value: "Move",backColor:"#B8860B"  },
+    { type: "str", value: "-",backColor:"#B8860B"  },
+    { type: "str", value: "&",backColor:"#B8860B"  },
+    { type: "str", value: ".",backColor:"#B8860B"  },
+    { type: "str", value: "/",backColor:"#B8860B"  }
 
-];
+];;
 
 let initialFromLength = 10;
 let fromLength = initialFromLength;
-let strButtonView = 19;
+let strButtonView = 24;
 let toLength = fromLength + strButtonView;
 let total = arr.length;
 let isShow = false;
@@ -49,10 +86,10 @@ const createButton = () => {
     var buttonNum = '';
     arr.map((item, index) => {
         if (item.type === "str" && index >= fromLength && index <= toLength) {
-            buttonText += `<div class="col-4 col-md-2 pl-0 pr-1 mb-1"><button  onclick="getData('${item.value}')" type="button" class="btn btn-light btn-block ">${item.value} </button></div>`;
+            buttonText += `<div class="col col-md pl-0 pr-0 pb-0 mb-0 "><button onclick="getData('${item.value}')" type="button" class="btn btn-light btn-block button_custom_br">${item.value} </button></div>`;
         }
         else if (item.type === "num") {
-            buttonNum += `<div class="col-2 col-md pl-0 pr-1 mb-1"><button  onclick="getData('${item.value}')" type="button" class="btn btn-light btn-block">${item.value} </button></div>`;
+            buttonNum += `<div class="col col-md pl-0 pr-0 mb-0 "><button onclick="getData('${item.value}')" type="button" class="btn btn-light btn-block button_custom_br">${item.value} </button></div>`;
         }
     });
     document.getElementById("allNum").innerHTML = buttonNum;
@@ -61,26 +98,24 @@ const createButton = () => {
 const getData = (value) => {
     let existValue = createElement.value;
     let totalLength = 0 ;
-    let superscript = { 0: '\u2070', 1: '\u00B9', 2: '\u00B2', 3: '\u00B3', 4: '\u2074', 5: '\u2075', 6: '\u2076', 7: '\u2077', 8: '\u2078', 9: '\u2079' }
+    let superscript = { 0: '\u2070', 1: '\u00B9', 2: '\u00B2', 3: '\u00B3', 4: '\u2074', 5: '\u2075', 6: '\u2076', 7: '\u2077', 8: '\u2078', 9: '\u2079',P:'\u1d3e',M:'\u1d39',R:'\u1d3f', }
     let subscript = { 0: '\u2080', 1: '\u2081', 2: '\u2082', 3: '\u2083', 4: '\u2084', 5: '\u2085', 6: '\u2086', 7: '\u2087', 8: '\u2088', 9: '\u2089' }
     let isNum = Number(value);
     console.log(isNum);
     if (value == 'space') {
         createElement.value = `${existValue} `;
     }
-    else if (isNum || isNum == 0) {
-        createElement.value = `${existValue}${superscript[isNum]}`;
+    else if (isNum || isNum == 0 || value == 'P' || value == 'M' || value == 'R') {
+        createElement.value = `${existValue}${superscript[value]}`;
+        totalLength = `${existValue}${value}`.length;
     } else {
         if (existValue == '') {
             createElement.value = `${existValue}${value}`;
+            totalLength = value.length;
         } else {
             createElement.value = `${existValue}, ${value}`;
+            totalLength = `${existValue}, ${value}`.length;
         }
-    }
-    if (existValue == '') {
-        totalLength = value.length;
-    } else {
-        totalLength = `${existValue}, ${value}`.length;
     }
     selectionStart = totalLength
     selectionEnd = totalLength
